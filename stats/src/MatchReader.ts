a57c24386feb44072071d9cd0f1134e28894fa7a
ryan <fswklotto@gmail.com>
2020-02-21 22:31:41 -0500

    minor update

--- stats/src/MatchReader.ts
+++ stats/src/MatchReader.ts
@@ -10,3 +10,4 @@ export class MatchReader extends CsvFileReader<MatchData> {
       return [dateStringToDate(row[0]), row[1], row[2], parseInt(row[3]), parseInt(row[4]), row[5] as MatchResult, row[6]];
     }
   }
+}
