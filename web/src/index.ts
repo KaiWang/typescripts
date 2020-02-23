156345cccc2ac100dd822fe1c99ce25e671e44ee
ryan <fswklotto@gmail.com>
2020-02-23 10:59:25 -0500

    minor update

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,7 +1,6 @@
 import { User } from './models/User';
 
 let user = new User({ id: 2, age: 40 });
-user.set({ id: 2, age: 41 });
 
 console.log(user);
 
--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -1,5 +1,6 @@
 import { Eventing } from './Eventing';
 import { Sync } from './Sync';
+import { Attributes } from './Attributes';
 
 export interface UserProps {
   id?: number;
@@ -10,13 +11,9 @@ export interface UserProps {
 export class User {
   public events: Eventing = new Eventing();
   public syncs = new Sync<UserProps>();
+  public attribute: Attributes<UserProps>;
 
-  constructor(private data: UserProps) {}
-  get(propName: string): string | number {
-    return this.data[propName];
-  }
-
-  set(update: UserProps): void {
-    Object.assign(this.data, update);
+  constructor(data: UserProps) {
+    this.attribute = new Attributes<UserProps>(data);
   }
 }
